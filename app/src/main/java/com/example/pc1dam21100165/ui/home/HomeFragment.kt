9af50a33bd1cf040effcb8183cdd7275bce17cd5
Joaquin <joaq11webmovil@gmail.com>
2024-10-05 10:24:00 -0500

    Pregunta 4

--- app/src/main/java/com/example/pc1dam21100165/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pc1dam21100165/ui/home/HomeFragment.kt
@@ -4,39 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pc1dam21100165.R
 import com.example.pc1dam21100165.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val equipos = arrayOf(
+            "FC Barcelona",
+            "Real Madrid",
+            "Manchester United",
+            "Bayern Munich",
+            "Liverpool",
+            "Esan FC",
+            "Juventus",
+            "Inter de Milán"
+        )
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val listView: ListView = view.findViewById(R.id.lvEquipos)
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, equipos)
+        listView.adapter = adapter
+
+        return view
         }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
