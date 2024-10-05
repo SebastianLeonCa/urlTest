7ae7fffcc95c54fc15fe607e619e36546aeaf88e
Joaquin <joaq11webmovil@gmail.com>
2024-10-05 10:09:38 -0500

    Pregunta 3

--- app/src/main/java/com/example/pc1dam21100165/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/pc1dam21100165/ui/slideshow/SlideshowFragment.kt
@@ -4,39 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.pc1dam21100165.R
 import com.example.pc1dam21100165.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
-
-    private var _binding: FragmentSlideshowBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val view: View = inflater.inflate(R.layout.fragment_slideshow, container, false)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val btCalcular: Button = view.findViewById(R.id.btnCalcular)
+        val etKilometros: EditText = view.findViewById(R.id.etKilometros)
+        val etMinutos: EditText = view.findViewById(R.id.etMinutos)
+        val txtResult: TextView = view.findViewById(R.id.textResult)
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+
+        btCalcular.setOnClickListener {
+            val kilometrosText = etKilometros.text.toString()
+            val minutosText = etMinutos.text.toString()
+
+            val kilometros = kilometrosText.toDouble()
+            val minutos = minutosText.toDouble()
+
+            val resultado = kilometros / minutos
+
+            txtResult.text = "$resultado km/min"
         }
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return view
     }
 }
